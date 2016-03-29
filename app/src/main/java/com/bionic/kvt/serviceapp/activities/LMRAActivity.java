cfd4943cf85e85e2d4aa826f0f23d6c342653596
Alexander Mylnikov <sasha.mylnikov@gmail.com>
2016-03-30 01:00:19 +0300

    EN2016-15: LMRA Report

--- app/src/main/java/com/bionic/kvt/serviceapp/activities/LMRAActivity.java
+++ app/src/main/java/com/bionic/kvt/serviceapp/activities/LMRAActivity.java
@@ -1,12 +1,15 @@
 package com.bionic.kvt.serviceapp.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.bionic.kvt.serviceapp.R;
+import com.bionic.kvt.serviceapp.adapters.LMRAAdapter;
+import com.bionic.kvt.serviceapp.models.LMRA;
 
 import java.util.ArrayList;
 
@@ -17,7 +20,25 @@ public class LMRAActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lmra);
 
-        ArrayList<View> mViews = new ArrayList<View>();
+        this.setTitle(R.string.lmra_title);
+
+        final ArrayList<LMRA> lmraList = new ArrayList<LMRA>();
+        final LMRAAdapter lmraAdapter = new LMRAAdapter(this,lmraList);
+        ListView listView = (ListView) findViewById(R.id.lmra_list);
+        listView.setAdapter(lmraAdapter);
+
+        LMRA newLMRA = new LMRA("Test1", "test description");
+        LMRA newLMRA2 = new LMRA("Test2", "test description");
+        LMRA newLMRA3 = new LMRA("Test3", "test description");
+        LMRA newLMRA4 = new LMRA("Test4", "test description");
+        LMRA newLMRA5 = new LMRA("Test5", "test description");
+        lmraAdapter.add(newLMRA);
+        lmraAdapter.add(newLMRA2);
+        lmraAdapter.add(newLMRA3);
+        lmraAdapter.add(newLMRA4);
+        lmraAdapter.add(newLMRA5);
+
+
 
     }
 }
