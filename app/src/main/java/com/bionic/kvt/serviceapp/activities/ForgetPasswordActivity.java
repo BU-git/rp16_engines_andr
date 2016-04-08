9c28ca3d3ed7b2ba43a26f240939a5fed39926b9
Alexander Mylnikov <sasha.mylnikov@gmail.com>
2016-04-08 10:47:32 +0300

    EN2016-22: javamail functionality

--- app/src/main/java/com/bionic/kvt/serviceapp/activities/ForgetPasswordActivity.java
+++ app/src/main/java/com/bionic/kvt/serviceapp/activities/ForgetPasswordActivity.java
@@ -1,14 +1,23 @@
 package com.bionic.kvt.serviceapp.activities;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.bionic.kvt.serviceapp.BuildConfig;
 import com.bionic.kvt.serviceapp.R;
 import com.bionic.kvt.serviceapp.helpers.HeaderHelper;
+import com.bionic.kvt.serviceapp.helpers.MailHelper;
 
 public class ForgetPasswordActivity extends AppCompatActivity {
     public static final String TAG = ForgetPasswordActivity.class.getName();
+    private MailHelper mailHelper;
+    private boolean sentStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,73 @@ public class ForgetPasswordActivity extends AppCompatActivity {
         HeaderHelper headerHelper = new HeaderHelper(this);
         headerHelper.setHeader();
 
+        final AutoCompleteTextView mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+
         Button button = (Button) findViewById(R.id.email_forget_pass_button);
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Reset errors.
+                    mEmailView.setError(null);
+
+                    // Store values at the time of the login attempt.
+                    String email = mEmailView.getText().toString();
+
+                    boolean cancel = false;
+                    View focusView = null;
+
+                    // Check for a valid email address.
+                    if (TextUtils.isEmpty(email)) {
+                        mEmailView.setError(getString(R.string.error_field_required));
+                        focusView = mEmailView;
+                        cancel = true;
+                    } else if (!isEmailValid(email)) {
+                        mEmailView.setError(getString(R.string.error_invalid_email));
+                        focusView = mEmailView;
+                        cancel = true;
+                    }
+
+                    if (cancel) {
+                        // There was an error; don't attempt login and focus the first
+                        // form field with an error.
+                        focusView.requestFocus();
+                    } else {
+                        mailHelper = new MailHelper();
+                        mailHelper.setRecepient(email);
+                        mailHelper.setBody(getText(R.string.forget_password_body).toString());
+                        mailHelper.setSubject(getText(R.string.forget_password_subject).toString());
+
+                        new SendMail().execute();
+                        if (sentStatus){
+                            Toast.makeText(ForgetPasswordActivity.this, getText(R.string.success_email_toast), Toast.LENGTH_SHORT);
+                        } else {
+                            Toast.makeText(ForgetPasswordActivity.this, getText(R.string.no_connection),Toast.LENGTH_SHORT);
+                        }
+
+                    }
+                }
+            });
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        //Email Contains @, // TODO: 4/8/2016 replace with a valid validator
+        return email.contains("@");
+    }
+
+    private class SendMail extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                mailHelper.send();
+                sentStatus = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                sentStatus = false;
+            }
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/com/bionic/kvt/serviceapp/helpers/MailHelper.java
@@ -0,0 +1,101 @@
+package com.bionic.kvt.serviceapp.helpers;
+
+/**
+
+ */
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.bionic.kvt.serviceapp.BuildConfig;
+
+import java.util.Date;
+import java.util.Properties;
+import javax.activation.CommandMap;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.activation.MailcapCommandMap;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+
+public class MailHelper extends javax.mail.Authenticator {
+
+    private String TAG = MailHelper.class.getName();
+
+    private Properties props;
+    private String recepient;
+    private String subject;
+
+    public String getRecepient() {
+        return recepient;
+    }
+
+    public void setRecepient(String recepient) {
+        this.recepient = recepient;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    private String body;
+
+    public MailHelper() {
+        props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+    }
+
+
+    public void send(){
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(BuildConfig.EMAIL_FROM,BuildConfig.EMAIL_PASSWORD);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(BuildConfig.EMAIL_FROM));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(recepient));
+            message.setSubject(subject);
+            message.setText(body);
+
+            Transport.send(message);
+
+            Log.d(TAG,"The message was sent successfully to " + recepient);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
