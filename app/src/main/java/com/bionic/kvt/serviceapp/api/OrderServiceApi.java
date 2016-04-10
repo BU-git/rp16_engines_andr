be4039367b226e3c9c516dfcf94935ecce2e9f42
dkrivoruchko@gmail.com <dkrivoruchko@gmai.com>
2016-04-10 11:56:21 +0300

    EN2016-82: Order POLO & API

--- app/src/main/java/com/bionic/kvt/serviceapp/api/OrderServiceApi.java
+++ app/src/main/java/com/bionic/kvt/serviceapp/api/OrderServiceApi.java
@@ -16,18 +16,21 @@ public interface OrderServiceApi {
 
     // Request list of all Users currently available in system
     // URL format /users
+    // Expecting JSON
     @GET("/users")
     Call<List<User>> getAllUsers();
 
 
     // Request list of all Orders currently available for {user} in brief format
     // URL format /orders/brief?user={email}
+    // Expecting JSON
     @GET("/orders/brief")
     Call<List<OrderBrief>> getOrdersByUser(@Query("user") String email);
 
 
     // Request one order with {number} for {user}
     // URL format /orders/{number}?user={email}
+    // Expecting JSON
     @GET("/orders/{number}")
     Call<Order> getOrdersByUser(@Path("number") int number, @Query("user") String email);
 
